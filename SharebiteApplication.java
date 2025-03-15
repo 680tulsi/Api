f53c28555d0db59da9fecadbc84823d2d541f929
Tulsisingh <163563115+680tulsi@users.noreply.github.com>
2025-03-15 14:53:36 +0530

    Add files via upload

---
+++ SharebiteApplication.java
@@ -0,0 +1,12 @@
+package com.example.sharebite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharebiteApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SharebiteApplication.class, args);
+		System.out.println("🚀 ShareBite Application Started Successfully!");
+	}
+}
